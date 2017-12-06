fc0d0d4338f360b9ccf51c7beff45d3a04f60548
Natalie Wyll <nataliewyll@yahoo.com>
2017-12-06 16:56:41 -0600

    checkpoint-2: list of chat rooms displayed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
-
+import RoomList from './components/RoomList/RoomList'
 
 // Initialize Firebase
 var config = {
@@ -18,37 +18,41 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-        {/* Sidebar */}
-        <div className="sidebar">
-          <div className="sidebar-header">
-            <h1 className="sidebar-title">ChatterBox</h1>
-            {/* New Room Button */}
-          </div>
-          <div className="sidebar-body">
-            <p className="room-name">Room Name</p>
-          </div>
-        </div>
-
-        {/* ChatSpace */}
-        <div className="chatspace">
-          <div class="chatspace-header">
-            <h2>Current Room Name</h2>
-          </div>
-          <div class="message-list container">
-            <div class="message-item">
-              <p class="message-username">message.username
-                <span class="message-timestamp">message.sentAt</span>
-              </p>
-              <p class="message-content">message.content</p>
+        <div className="css-table">
+          {/* Sidebar */}
+          <div className="sidebar">
+            <div className="sidebar-header">
+              <h1 className="sidebar-title">ChatterBox</h1>
+              {/* New Room Button */}
+              <button type="button" className="new-room">New Room</button>
+            </div>
+            <div className="sidebar-body">
+              {/* Room List - pass firebase in props */}
+              <RoomList firebase={firebase}/>
             </div>
           </div>
-          <div class="new-message-field">
-            <input type="text" name="input" placeholder="Type your message here" class="message-box" style="width:500px;" />
-              <button class="btn-new-message" type="button">Send message</button>
-            </div>
-        </div>
 
+          {/* ChatSpace */}
+          <div className="chatspace">
+            <div className="chatspace-header">
+              <h2>Current Room Name</h2>
+            </div>
+            <div className="message-list container">
+              {/* Messages */}
+              <div className="message-body">
+                <p className="username">message.username
+                  <span className="timestamp">   message.sentAt</span>
+                </p>
+                <p className="content">message.content</p>
+              </div>
+              {/* Messages */}
+            </div>
+            <div className="new-message-field">
+              <input type="text" name="input" placeholder="Type your message here" />
+                <button className="btn-new-message" type="button">Send message</button>
+              </div>
+          </div>
+        </div>
       </div>
     );
   }
