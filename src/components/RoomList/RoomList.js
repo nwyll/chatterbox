9243be21c50382f0783af5c8831a38c84716d374
Natalie Wyll <nataliewyll@yahoo.com>
2017-12-07 12:20:26 -0600

    can create new rooms

--- src/components/RoomList/RoomList.js
+++ src/components/RoomList/RoomList.js
@@ -5,7 +5,8 @@ class RoomList extends Component {
     super(props);
 
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ''
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -19,11 +20,38 @@ class RoomList extends Component {
     });
   }
 
+  newRoomName =  (event) => {
+    this.setState({ newRoomName: event.target.value});
+  }
+
+  createRoom = () => {
+    this.roomsRef.push({
+      name: this.state.newRoomName
+    });
+  }
+
   render() {
     return (
-      this.state.rooms.map( room =>
-        <p key={room.key} className="room-name">{ room.name }</p>
-        )
+      <div className="room-list">
+        {
+          this.state.rooms.map( (room) =>
+            <p key={room.key} className="room-name">{ room.name }</p>
+          )
+        }
+
+        <div className="room-form">
+          {/* <h3>Create a new room:</h3> */}
+          <form>
+            <label>
+              Create a new room:
+              <input type="text" placeholder="Enter room name"
+                value={this.state.newRoomName}
+                onChange={this.newRoomName} />
+            </label><br/><br/>
+            <button type="submit" onClick={this.createRoom}>Create Room</button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
